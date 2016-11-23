39830923f2f04e0e753d1f868e5c487b21e36cae
Daniel Rosenberg <dan.vic.ros@gmail.com>
2016-11-23 15:36:53 +0100

    final version

---
+++ Labb11/Labb11/Runtime.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb11
+{
+    
+
+    class Runtime
+    {
+        public delegate void Analyzenumber(int number);
+        public event Analyzenumber NumberInput;
+
+        public void Start()
+        {
+            Console.WriteLine("Skriv in ett heltal: ");
+            int uInput = int.Parse(Console.ReadLine());
+
+            NumberInput += new Analyzenumber(IsEven);
+            NumberInput += new Analyzenumber(IsDivisibleByThree);
+            NumberInput += new Analyzenumber(IsPrimeNumber);
+            OnNumberInput(uInput);
+
+        }
+
+        private void OnNumberInput(int uInput)
+        {
+            NumberInput?.Invoke(uInput);
+        }
+
+        public void IsDivisibleByThree(int number)
+        {
+            Console.WriteLine("{0} {1} delbart med tre", number, number % 3 == 0 ? "är" : "är inte" );
+        }
+
+        public void IsPrimeNumber(int number)
+        {
+            bool isPrime = true;
+
+            for(int i = 2; i < number; i++)
+            {
+                if(number % i == 0)
+                {
+                    isPrime = false;
+               
+                }
+            }
+
+            Console.WriteLine("{0} {1} ett primtal", number, isPrime == true ? "är" : "är inte");
+        }
+        
+        public void IsEven(int number)
+        {
+            Console.WriteLine("{0} {1} ett jämnt tal", number, number % 2 == 0 ? "är" : "är inte");
+        }
+        
+    }
+}
